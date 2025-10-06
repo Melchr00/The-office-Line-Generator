7893ce185fc98028cf706b5a58fef27bf2ae5708
Melkchri100 <melkchri100@student.kau.se>
2025-10-06 21:55:31 +0200

    created API routes plan

---
+++ admin-api/utils/subscriptionManagement.js
@@ -0,0 +1,10 @@
+// TODO 
+// Get all roles  (GET /api/admin/roles)
+// Get specific role (GET /api/admin/roles/:roleName)
+// Get user roles (GET /api/admin/users/:userName/roles)
+// Assign role (POST /api/admin/users/:userName/roles/:roleName)
+// Remove role (DELETE /api/admin/users/:userName/roles/:roleName)
+// Update subscription (Handle upgrde/downgrade) (PUT /api/admin/users/:userName/subscription)
+// Get subscription status (GET /api/admin/users/:userName/subscription)
+
+
---
+++ admin-api/utils/userManagement.js
@@ -0,0 +1,10 @@
+// TODO
+// Get all users (GET /api/admin/users)
+// Get User by ID (GET /api/admin/users/id/:userId)
+// Update user info (GET /api/admin/users/:userName)
+// Create user (POST api/admin/createUser)
+// Delete user (DELETE api/admin/users/:userName)
+// Reset password (PUT /api/admin/users/:userName/reset-password)
+// Verify email (PUT /api/admin/users/:userName/verify-email)
+
+
