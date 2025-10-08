23685f3c72659035d8bdd726a40e63e5c0a4be68
Melkchri100 <melkchri100@student.kau.se>
2025-10-08 18:17:43 +0200

    fixed issue with ERR_CONNECTION_REFUSED

--- the-office-frontend/vite.config.js
+++ the-office-frontend/vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
     tailwindcss(),
   ],
    server: {
+     host: '127.0.0.1',
+     port: 5173,
     historyApiFallback: true, // ensures React Router handles unknown routes
   }
 })
